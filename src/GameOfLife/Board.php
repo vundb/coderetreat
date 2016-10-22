949798fd389a485e6f4956957a9eb253cd57d386
Viktor Breitenbuecher <vic.b@outlook.com>
2016-10-22 16:51:09 +0200

    Initialize board with dead cells.
    ml

--- src/GameOfLife/Board.php
+++ src/GameOfLife/Board.php
@@ -10,12 +10,46 @@ namespace GameOfLife;
 class Board
 {
     /**
+     * @var Cell[][]
+     */
+    private $field;
+
+    /**
      * @param Coordinate $coord
      *
      * @return bool
      */
     public function getStateOnField(Coordinate $coord)
     {
-        return false;
+        return $this->field[$coord->getX()][$coord->getY()]->isAlive();
+    }
+
+    /**
+     * Board constructor.
+     *
+     * @param int $x
+     * @param int $y
+     */
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+
+        $this->field = [];
+
+        $this->initField();
+    }
+
+    /**
+     * Initialize field and assign cells.
+     */
+    private function initField()
+    {
+        for ($i = 0; $i < $this->x; $i++) {
+            for ($j = 0; $j < $this->y; $j++) {
+
+                $this->field[$i][$j] = new Cell();
+            }
+        }
     }
 }
