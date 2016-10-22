0837fa94bb572ae550af832ed4a8b87d553302d6
Viktor Breitenbuecher <vic.b@outlook.com>
2016-10-22 14:49:14 +0200

    pblom-vs-vundb

--- src/GameOfLife/Cell.php
+++ src/GameOfLife/Cell.php
@@ -10,4 +10,63 @@ namespace GameOfLife;
 class Cell
 {
 
+    private $state;
+
+    private $nextState;
+
+    private $neighbours = [];
+
+    public function __construct($state = false)
+    {
+        $this->state = $state;
+        $this->nextState = false;
+    }
+
+    /**
+     * @param array $neighbours
+     *
+     * @return bool
+     */
+    public function setNeighbours(array $neighbours)
+    {
+        $this->neighbours = $neighbours;
+        return true;
+    }
+
+    public function countLivingNeighbours()
+    {
+        $cellsWhichAreStillAliveAndThisNameIsNotToLong = [];
+        foreach ($this->neighbours as $neighbour) {
+            if ($neighbour->state) {
+                array_push($cellsWhichAreStillAliveAndThisNameIsNotToLong, $neighbour);
+            }
+        }
+        return count($cellsWhichAreStillAliveAndThisNameIsNotToLong);
+    }
+
+    public function setAlive()
+    {
+        $this->state = true;
+        return true;
+    }
+
+    public function setDead()
+    {
+        $this->state = false;
+    }
+
+    public function calculateNextState()
+    {
+        var_dump($this->neighbours); die();
+        if ($this->state && 3 === $this->countLivingNeighbours()) {
+            $this->nextState = true;
+        }
+
+        $this->nextState = false;
+    }
+
+    public function getNextState()
+    {
+        return $this->nextState;
+    }
 }
--- src/GameOfLifeTest/CellTest.php
+++ src/GameOfLifeTest/CellTest.php
@@ -2,6 +2,8 @@
 
 namespace GameOfLifeTest;
 
+use GameOfLife\Cell;
+
 /**
  * Class GameOfLifeTest
  *
@@ -9,11 +11,97 @@ namespace GameOfLifeTest;
  */
 class CellTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     *
-     */
-    public function testNothing()
+    public function testCountLivingNeighbours()
     {
-        $this->assertTrue(true);
+        $cell = new Cell();
+
+        $cells = $this->createNeighbours(8);
+        $cells[0]->setAlive(true);
+        $cells[1]->setAlive(true);
+
+        $cell->setNeighbours($cells);
+
+        $this->assertEquals(2, $cell->countLivingNeighbours());
+    }
+
+    public function testsetNeighbours()
+    {
+        $cell = new Cell();
+
+        $cells = $this->createNeighbours(8);
+        $cells[0]->setAlive(true);
+        $cells[1]->setAlive(true);
+
+        $return = $cell->setNeighbours($cells);
+        $this->assertTrue($return);
+    }
+
+    public function testSetAlive()
+    {
+        $cell = new Cell(true);
+
+        $cells = $this->createNeighbours(8);
+        $cells[0]->setAlive(true);
+        $cells[1]->setAlive(true);
+        $cells[3]->setAlive(true);
+
+        $cell->setNeighbours($cells);
+
+        $this->assertEquals(3, $cell->countLivingNeighbours());
+    }
+
+    public function testSetDead()
+    {
+        $cell = new Cell(true);
+
+        $cells = $this->createNeighbours(3);
+        $cells[0]->setAlive(true);
+        $cells[0]->setDead(true);
+
+        $cell->setNeighbours($cells);
+
+        $this->assertEquals(0, $cell->countLivingNeighbours());
+    }
+
+    public function testLivingCellWillBeDeadWithDeadNeighbours()
+    {
+        $cell = new Cell(true);
+        $cell->setNeighbours(
+            $this->createNeighbours(8)
+        );
+
+        $cell->calculateNextState();
+
+        $this->assertFalse($cell->getNextState());
+    }
+
+    public function testCellWillBeRescuedIfThreeNeighboursAreAlive()
+    {
+        $cell = new Cell(true);
+        $neighbours = $this->createNeighbours(8);
+        $neighbours[0]->setAlive();
+        $neighbours[1]->setAlive();
+        $neighbours[2]->setAlive();
+        $cell->setNeighbours($neighbours);
+
+        $cell->calculateNextState();
+
+        $this->assertTrue($cell->getNextState());
+    }
+
+    #######
+
+    /**
+     * @param $count
+     *
+     * @return Cell[]
+     */
+    private function createNeighbours($count)
+    {
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            $result[] = new Cell();
+        }
+        return $result;
     }
 }
