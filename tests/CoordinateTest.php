992d47cf460acd1301db73723d434559c0159108
Viktor Breitenbuecher <vic.b@outlook.com>
2016-10-22 22:46:35 +0200

    Reorganize files. Add coverage report.

---
+++ tests/CoordinateTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace GameOfLifeTest;
+
+use GameOfLife\Coordinate;
+
+/**
+ * Class CoordinateTest
+ *
+ * @package GameOfLifeTest
+ */
+class CoordinateTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \GameOfLife\Coordinate
+     */
+    public function testConstructor()
+    {
+        $x = 11;
+        $y = 22;
+
+        $coord = new Coordinate($x, $y);
+
+        $this->assertEquals($x, $coord->getX());
+        $this->assertEquals($y, $coord->getY());
+    }
+}
